742a4554835c01b0243139face900905d149952c
RofiArrahman <148022347+RofiArrahman@users.noreply.github.com>
2023-11-10 02:40:48 +0700

    Add files via upload

---
+++ latihan4A.php
@@ -0,0 +1,20 @@
+<?php
+$balon =["Hijau","Kuning","Kelabu","Merah Muda"];
+$balon[] = "Biru";
+
+$balon[0];
+$balon[1];
+$balon[2];
+$balon[3];
+$balon[4];
+
+echo"Balonku ada lima <br>";
+echo"Rupa-rupa warnanya<br>";
+echo "$balon[0], $balon[1], $balon[2], $balon[3], dan $balon[4]<br>";
+echo "Meletus balon $balon[0] DOORRR!!!!<br>";
+echo "Hatiku sangat kacau<br>";
+
+?>
+<style>
+    
+</style>
---
+++ latihan4B.php
@@ -0,0 +1,14 @@
+<?php
+$negara =["Indonesia","Singapura","Malaysia","Brunei","Thailand"];
+
+echo"<b>Daftar Negara ASEAN Awal </b>:";
+echo "<br>";
+   foreach ($negara as $asean) {
+    echo "<li>$asean";
+   }
+echo"<br><br>";
+$baru = array_push($negara, "Laos","Filipina","Myanmar");
+echo "<b>Daftar Negara ASEAN Baru :</b>";
+foreach ($negara as $asean) {
+    echo "<li>$asean";
+   }
